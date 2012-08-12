ea7c1f8f5c02e4ae0aee359f6ce68fc1b78498c4
Matthew Davidson <davidsm9@nationwide.com>
2012-08-11 22:38:03 -0400

    Started working in MVC pattern

--- javascript/spec/calculator_spec.js
+++ javascript/spec/calculator_spec.js
@@ -1,8 +1,11 @@
 describe("CalculatorController", function() {
-	it("", function(){
-		affix("button#keyPad_btn0[value=0]");
-		var calculatorForm = CalculatorView;
-		var zeroButton = calculatorForm.getZeroField();
-		expect(zeroButton.val()).toBe('0');
+	it("clears the display when the clear button is pressed", function(){
+		affix("input#keyPad_UserInput[value=5]");
+        affix("button#keyPad_btnClr");
+		var calculator = CalculatorController;
+	    var calculatorView = calculator.getView();
+
+        calculatorView.getClearButton().trigger("click");
+		expect(calculatorView.getScreenValue()).toBe('0');
 	});
 });
--- javascript/src/calculator.js
+++ javascript/src/calculator.js
@@ -1,12 +1,31 @@
 var CalculatorView= function() {
 	var calculatorView = {};
+    calculatorView.init = function(){
+
+    };
+
+    calculatorView.getClearButton = function(){
+        return $('#keyPad_btnClr');
+    };
+    calculatorView.getScreenValue = function(){
+        return $('#keyPad_UserInput').val();
+    };
+
 	return calculatorView;
 }();
 
 var CalculatorController= function() {
     var calculatorController = {};
+    //var calculatorView = CalculatorView;
+
+
+    calculatorController.getView = function(){
+        return calculatorView;
+    };
     return calculatorController;
 }();
+
+
 
 
 
