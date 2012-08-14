beb71d50ce12073476ef24a5e0270be44a8248e3
Matthew Davidson <davidsm9@nationwide.com>
2012-08-14 00:19:47 -0400

    MD's attempt at the MVC calculator

--- javascript/spec/calculator_spec.js
+++ javascript/spec/calculator_spec.js
@@ -2,10 +2,33 @@ describe("CalculatorController", function() {
 	it("clears the display when the clear button is pressed", function(){
 		affix("input#keyPad_UserInput[value=5]");
         affix("button#keyPad_btnClr");
-		var calculator = CalculatorController;
-	    var calculatorView = calculator.getView();
+            var calculatorModel = new CalculatorModel();
+            var calculatorController = new CalculatorController(calculatorModel);
+            var calculatorView = new CalculatorView(calculatorModel,calculatorController);
+            calculatorView.initialize();
+            calculatorView.getClearButton().trigger("click");
+            expect(calculatorView.getInput().val()).toBe('0');
 
-        calculatorView.getClearButton().trigger("click");
-		expect(calculatorView.getScreenValue()).toBe('0');
 	});
+    it("adds 1 to the input screen when current value is 5",function(){
+        affix("input#keyPad_UserInput[value=5]");
+        affix("button#keyPad_btn1");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("5");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getOneButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('51');
+    });
+    it("adds 1 to the input screen when current value is 0",function(){
+        affix("input#keyPad_UserInput[value=0]");
+        affix("button#keyPad_btn1");
+        var calculatorModel = new CalculatorModel();
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getOneButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('1');
+    });
 });
--- javascript/src/calculator.js
+++ javascript/src/calculator.js
@@ -1,32 +1,108 @@
-var CalculatorView= function() {
-	var calculatorView = {};
-    calculatorView.init = function(){
+var CalculatorView= function(model,controller) {
+    //MVC
+    this.calculatorModel = model;
+    this.calculatorController = controller;
+    var _this = this;
 
+
+
+    this.getModel = function(){
+     return this.calculatorModel;
     };
-
-    calculatorView.getClearButton = function(){
+    this.getController = function(){
+      return this.calculatorController;
+    };
+    this.getClearButton = function(){
         return $('#keyPad_btnClr');
     };
-    calculatorView.getScreenValue = function(){
-        return $('#keyPad_UserInput').val();
+    this.getOneButton = function(){
+        return $('#keyPad_btn1');
     };
 
-	return calculatorView;
-}();
-
-var CalculatorController= function() {
-    var calculatorController = {};
-    //var calculatorView = CalculatorView;
-
-
-    calculatorController.getView = function(){
-        return calculatorView;
+    this.getInput = function(){
+        return $('#keyPad_UserInput');
     };
-    return calculatorController;
-}();
+    //event listener
+    this.calculatorModel.updated.attach(function () {
+        _this.getInput().val(_this.getModel().getInputValue());
+    });
+
+//initialize
+    this.initialize = function(){
+
+        this.getClearButton().click({controller: this.getController()}, clickClearButton);
+        this.getOneButton().click({controller: this.getController()}, clickOneButton);
+    };
+    function clickClearButton(event){
+        event.data.controller.clearInput();
+    }
+    function clickOneButton(event){
+        event.data.controller.appendNumber("1");
+    }
+};
+
+var CalculatorController= function(model) {
+    //MVC
+    this.calculatorModel = model;
+
+    this.getModel = function() {
+        return this.calculatorModel;
+    };
+    //members
+    this.clearInput= function(){
+        //localModel = this.getModel();
+        this.calculatorModel.setInputValue("0");
+        this.calculatorModel.setOperand("");
+        this.calculatorModel.setStoredValue("");
+    };
+    this.appendNumber= function(val){
+        if(this.calculatorModel.getInputValue() == "0"){
+            this.calculatorModel.setInputValue(val);
+        }else{
+            this.calculatorModel.setInputValue(this.calculatorModel.getInputValue().concat(val));
+        }
+    };
+
+};
+
+var CalculatorModel= function(){
+
+    this.inputValue = "";
+    this.operand = "";
+    this.storedvalue = "";
+
+    //members
+    this.setInputValue = function(xInput){
+        this.inputValue = xInput;
+        this.updated.notify();
+    };
+    this.getInputValue = function(){
+        return this.inputValue;
+    };
+    this.setOperand = function(xOperand){
+        this.operand = xOperand;
+    };
+    this.setStoredValue = function(xValue){
+        this.storedvalue = xValue;
+    };
+
+    this.updated = new Event();
+};
 
 
 
+var Event = function (sender) {
+    this._sender = sender;
+    this._listeners = [];
+};
 
-
-
+Event.prototype = {
+    attach : function (listener) {
+        this._listeners.push(listener);
+    },
+    notify : function (args) {
+        for (var i = 0; i < this._listeners.length; i++) {
+            this._listeners[i](this._sender, args);
+        }
+    }
+};
