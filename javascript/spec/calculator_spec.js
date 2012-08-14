b19ae3a744fcfbd37800ce27bce84a56dea34ead
Matthew Davidson <davidsm9@nationwide.com>
2012-08-14 01:36:05 -0400

    Added buttons two through 0, . and -/+. just need operands.

--- javascript/spec/calculator_spec.js
+++ javascript/spec/calculator_spec.js
@@ -3,13 +3,18 @@ describe("CalculatorController", function() {
 		affix("input#keyPad_UserInput[value=5]");
         affix("button#keyPad_btnClr");
             var calculatorModel = new CalculatorModel();
+            calculatorModel.setInputValue("0");
             var calculatorController = new CalculatorController(calculatorModel);
             var calculatorView = new CalculatorView(calculatorModel,calculatorController);
             calculatorView.initialize();
             calculatorView.getClearButton().trigger("click");
             expect(calculatorView.getInput().val()).toBe('0');
+            calculatorModel = null;
+            calculatorController = null;
+            calculatorView = null;
 
 	});
+    //One
     it("adds 1 to the input screen when current value is 5",function(){
         affix("input#keyPad_UserInput[value=5]");
         affix("button#keyPad_btn1");
@@ -20,15 +25,380 @@ describe("CalculatorController", function() {
         calculatorView.initialize();
         calculatorView.getOneButton().trigger("click");
         expect(calculatorView.getInput().val()).toBe('51');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
     });
     it("adds 1 to the input screen when current value is 0",function(){
         affix("input#keyPad_UserInput[value=0]");
         affix("button#keyPad_btn1");
         var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("0");
         var calculatorController = new CalculatorController(calculatorModel);
         var calculatorView = new CalculatorView(calculatorModel, calculatorController);
         calculatorView.initialize();
         calculatorView.getOneButton().trigger("click");
         expect(calculatorView.getInput().val()).toBe('1');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    //two
+    it("adds 2 to the input screen when current value is 5",function(){
+        affix("input#keyPad_UserInput[value=5]");
+        affix("button#keyPad_btn2");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("5");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getTwoButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('52');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    it("adds 2 to the input screen when current value is 0",function(){
+        affix("input#keyPad_UserInput[value=0]");
+        affix("button#keyPad_btn2");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("0");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getTwoButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('2');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    //three
+    it("adds 3 to the input screen when current value is 5",function(){
+        affix("input#keyPad_UserInput[value=5]");
+        affix("button#keyPad_btn3");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("5");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getThreeButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('53');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    it("adds 3 to the input screen when current value is 0",function(){
+        affix("input#keyPad_UserInput[value=0]");
+        affix("button#keyPad_btn3");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("0");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getThreeButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('3');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    //four
+    it("adds 4 to the input screen when current value is 5",function(){
+        affix("input#keyPad_UserInput[value=5]");
+        affix("button#keyPad_btn4");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("5");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getFourButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('54');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    it("adds 4 to the input screen when current value is 0",function(){
+        affix("input#keyPad_UserInput[value=0]");
+        affix("button#keyPad_btn4");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("0");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getFourButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('4');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    //five
+    it("adds 5 to the input screen when current value is 5",function(){
+        affix("input#keyPad_UserInput[value=5]");
+        affix("button#keyPad_btn5");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("5");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getFiveButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('55');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    it("adds 5 to the input screen when current value is 0",function(){
+        affix("input#keyPad_UserInput[value=0]");
+        affix("button#keyPad_btn5");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("0");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getFiveButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('5');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+//six
+    it("adds 6 to the input screen when current value is 5",function(){
+        affix("input#keyPad_UserInput[value=5]");
+        affix("button#keyPad_btn6");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("5");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getSixButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('56');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    it("adds 6 to the input screen when current value is 0",function(){
+        affix("input#keyPad_UserInput[value=0]");
+        affix("button#keyPad_btn6");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("0");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getSixButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('6');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+//seven
+    it("adds 7 to the input screen when current value is 5",function(){
+        affix("input#keyPad_UserInput[value=5]");
+        affix("button#keyPad_btn7");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("5");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getSevenButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('57');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    it("adds 7 to the input screen when current value is 0",function(){
+        affix("input#keyPad_UserInput[value=0]");
+        affix("button#keyPad_btn7");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("0");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getSevenButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('7');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+//eight
+    it("adds 8 to the input screen when current value is 5",function(){
+        affix("input#keyPad_UserInput[value=5]");
+        affix("button#keyPad_btn8");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("5");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getEightButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('58');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    it("adds 8 to the input screen when current value is 0",function(){
+        affix("input#keyPad_UserInput[value=0]");
+        affix("button#keyPad_btn8");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("0");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getEightButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('8');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+//nine
+    it("adds 9 to the input screen when current value is 5",function(){
+        affix("input#keyPad_UserInput[value=5]");
+        affix("button#keyPad_btn9");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("5");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getNineButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('59');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    it("adds 9 to the input screen when current value is 0",function(){
+        affix("input#keyPad_UserInput[value=0]");
+        affix("button#keyPad_btn9");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("0");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getNineButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('9');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+//zero
+    it("adds 0 to the input screen when current value is 5",function(){
+        affix("input#keyPad_UserInput[value=5]");
+        affix("button#keyPad_btn0");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("5");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getZeroButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('50');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    it("adds 0 to the input screen when current value is 0",function(){
+        affix("input#keyPad_UserInput[value=0]");
+        affix("button#keyPad_btn0");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("0");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getZeroButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('0');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+//Dot
+    it("adds . to the input screen when current value is 5",function(){
+        affix("input#keyPad_UserInput[value=5]");
+        affix("button#keyPad_btnDot");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("5");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getDotButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('5.');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    it("adds . to the input screen when current value is 0",function(){
+        affix("input#keyPad_UserInput[value=0]");
+        affix("button#keyPad_btnDot");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("0");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getDotButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('0.');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+//InverseSign
+    it("change the sign of the input to positive",function(){
+        affix("input#keyPad_UserInput[value=-5]");
+        affix("button#keyPad_btnInverseSign");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("-5");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getInverseSignButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('5');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    it("Change the sign of the input to negative",function(){
+        affix("input#keyPad_UserInput[value=5]");
+        affix("button#keyPad_btnInverseSign");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("5");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getInverseSignButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('-5');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
+    });
+    it("Does nothing when input is Zero",function(){
+        affix("input#keyPad_UserInput[value=0]");
+        affix("button#keyPad_btnInverseSign");
+        var calculatorModel = new CalculatorModel();
+        calculatorModel.setInputValue("0");
+        var calculatorController = new CalculatorController(calculatorModel);
+        var calculatorView = new CalculatorView(calculatorModel, calculatorController);
+        calculatorView.initialize();
+        calculatorView.getInverseSignButton().trigger("click");
+        expect(calculatorView.getInput().val()).toBe('0');
+        calculatorModel = null;
+        calculatorController = null;
+        calculatorView = null;
+
     });
 });
--- javascript/src/calculator.js
+++ javascript/src/calculator.js
@@ -18,6 +18,39 @@ var CalculatorView= function(model,controller) {
     this.getOneButton = function(){
         return $('#keyPad_btn1');
     };
+    this.getTwoButton = function(){
+        return $('#keyPad_btn2');
+    };
+    this.getThreeButton = function(){
+        return $('#keyPad_btn3');
+    };
+    this.getFourButton = function(){
+        return $('#keyPad_btn4');
+    };
+    this.getFiveButton = function(){
+        return $('#keyPad_btn5');
+    };
+    this.getSixButton = function(){
+        return $('#keyPad_btn6');
+    };
+    this.getSevenButton = function(){
+        return $('#keyPad_btn7');
+    };
+    this.getEightButton = function(){
+        return $('#keyPad_btn8');
+    };
+    this.getNineButton = function(){
+        return $('#keyPad_btn9');
+    };
+    this.getZeroButton = function(){
+        return $('#keyPad_btn0');
+    };
+    this.getDotButton = function(){
+        return $('#keyPad_btnDot');
+    };
+    this.getInverseSignButton = function(){
+        return $('#keyPad_btnInverseSign');
+    };
 
     this.getInput = function(){
         return $('#keyPad_UserInput');
@@ -32,6 +65,18 @@ var CalculatorView= function(model,controller) {
 
         this.getClearButton().click({controller: this.getController()}, clickClearButton);
         this.getOneButton().click({controller: this.getController()}, clickOneButton);
+        this.getTwoButton().click({controller: this.getController()}, clickTwoButton);
+        this.getThreeButton().click({controller: this.getController()}, clickThreeButton);
+        this.getFourButton().click({controller: this.getController()}, clickFourButton);
+        this.getFiveButton().click({controller: this.getController()}, clickFiveButton);
+        this.getSixButton().click({controller: this.getController()}, clickSixButton);
+        this.getSevenButton().click({controller: this.getController()}, clickSevenButton);
+        this.getEightButton().click({controller: this.getController()}, clickEightButton);
+        this.getNineButton().click({controller: this.getController()}, clickNineButton);
+        this.getZeroButton().click({controller: this.getController()}, clickZeroButton);
+        this.getDotButton().click({controller: this.getController()}, clickDotButton);
+        this.getInverseSignButton().click({controller: this.getController()}, clickInverseSignButton);
+
     };
     function clickClearButton(event){
         event.data.controller.clearInput();
@@ -39,6 +84,40 @@ var CalculatorView= function(model,controller) {
     function clickOneButton(event){
         event.data.controller.appendNumber("1");
     }
+    function clickTwoButton(event){
+        event.data.controller.appendNumber("2");
+    }
+    function clickThreeButton(event){
+        event.data.controller.appendNumber("3");
+    }
+    function clickFourButton(event){
+        event.data.controller.appendNumber("4");
+    }
+    function clickFiveButton(event){
+        event.data.controller.appendNumber("5");
+    }
+    function clickSixButton(event){
+        event.data.controller.appendNumber("6");
+    }
+    function clickSevenButton(event){
+        event.data.controller.appendNumber("7");
+    }
+    function clickEightButton(event){
+        event.data.controller.appendNumber("8");
+    }
+    function clickNineButton(event){
+        event.data.controller.appendNumber("9");
+    }
+    function clickZeroButton(event){
+        event.data.controller.appendNumber("0");
+    }
+    function clickDotButton(event){
+        event.data.controller.appendNumber(".");
+    }
+    function clickInverseSignButton(event){
+        event.data.controller.invertSign();
+    }
+
 };
 
 var CalculatorController= function(model) {
@@ -56,12 +135,21 @@ var CalculatorController= function(model) {
         this.calculatorModel.setStoredValue("");
     };
     this.appendNumber= function(val){
-        if(this.calculatorModel.getInputValue() == "0"){
-            this.calculatorModel.setInputValue(val);
+        if(this.calculatorModel.getInputValue() == "0" && !(val == ".")){
+                this.calculatorModel.setInputValue(val);
         }else{
             this.calculatorModel.setInputValue(this.calculatorModel.getInputValue().concat(val));
         }
     };
+    this.invertSign = function(){
+        if(this.calculatorModel.getInputValue().charAt(0) == "-"){
+            this.calculatorModel.setInputValue(this.calculatorModel.getInputValue().replace("-",""));
+        }else{
+            if(!(this.calculatorModel.getInputValue() == "0")){
+                this.calculatorModel.setInputValue("-".concat(this.calculatorModel.getInputValue()));
+            }
+        }
+    }
 
 };
 
